f955c9a645c2a9d11a8a3c25bc343a77720b7610
yyl <yuyl@taixin.cn>
2014-11-18 16:37:25 +0800

    16.37

--- android/txmediaplayer/obj/local/armeabi-v7a/objs/txutil_c/android/loghelp.o.d
+++
@@ -1,8 +0,0 @@
-/home/yyl/github/txplayer/android/txmediaplayer/obj/local/armeabi-v7a/objs/txutil_c/android/loghelp.o: \
- /home/yyl/github/txplayer/android/txmediaplayer/jni/txmedia/txutil/android/loghelp.c \
- /home/yyl/github/txplayer/android/txmediaplayer/jni/txmedia/txutil/android/loghelp.h \
- /home/yyl/javatool/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/log.h
-
-/home/yyl/github/txplayer/android/txmediaplayer/jni/txmedia/txutil/android/loghelp.h:
-
-/home/yyl/javatool/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/log.h:
